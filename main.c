adba7d04869050023c7c1a3a6ad769bee45f0240
Elton J <eltonjanduhy@gmail.com>
2021-12-28 23:39:13 -0300

    Background scroll pt2

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <gb/font.h>
 #include "map/background_map.c"
 #include "tiles/background_map_tiles.c"
 
 
+
 void main(){
 
-    set_bkg_data(0, 7, backgroud_map_tiles);
+    font_t min_font;
+
+    font_init();
+    min_font = font_load(font_min); //36 tile
+    font_set(min_font);
+
+    set_bkg_data(37, 7, backgroud_map_tiles);
     set_bkg_tiles(0, 0, 40, 18, background_map);
 
     SHOW_BKG;
